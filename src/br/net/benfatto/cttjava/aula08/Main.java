9fc00cb055dda123791725851943ea45c159b4a1
Murilo Rossi de Oliveira <murilo.rossi.oliveira@gmail.com>
2021-01-28 20:48:06 -0300

    Aula 08 - Arrays por referencia

--- src/br/net/benfatto/cttjava/aula08/Main.java
+++ src/br/net/benfatto/cttjava/aula08/Main.java
@@ -14,20 +14,20 @@ public class Main {
         array.print(arr);
 
         // busca maior valor no array
-        int maior = array.pegarMaior(arr);
-        System.out.println("\nMaior: " + maior); // 45
-
-        // busca menor valor no array
-        int menor = array.pegarMenor(arr);
-        System.out.println("\nMenor: " + menor); // 0
-
-        // calcula a media dos elementos do array
-        float media = array.pegarMedia(arr);
-        System.out.println("\nMedia: " + media); //
-
-        // busca total de zeros
-        int totalZeros = array.contarZeros(arr);
-        System.out.println("\nTotal de zeros: " + totalZeros);
+//        int maior = array.pegarMaior(arr);
+//        System.out.println("\nMaior: " + maior); // 45
+//
+//        // busca menor valor no array
+//        int menor = array.pegarMenor(arr);
+//        System.out.println("\nMenor: " + menor); // 0
+//
+//        // calcula a media dos elementos do array
+//        float media = array.pegarMedia(arr);
+//        System.out.println("\nMedia: " + media); //
+//
+//        // busca total de zeros
+//        int totalZeros = array.contarZeros(arr);
+//        System.out.println("\nTotal de zeros: " + totalZeros);
 
         // inverte o array
 //        List<Integer> arrayInvertido = array.inverter(arr);
@@ -35,18 +35,15 @@ public class Main {
 //        arrayInvertido.forEach(System.out::println);
 ////        array.print(arrayInvertido);
 //
-        array.print(arr);
-
         System.out.println("\n\n");
-        array.inverterEugenio(arr);
+//        array.inverterEugenio(arr);
 //
 //        System.out.println("\n\n");
 //        array.inverterGuilherme(arr);
 
-
         array.print(arr);
         System.out.println("\n\n");
-        int[] arrayInvertidoMurilo = array.inverterMurilo(new int[]{10, 23, 0, 4, 45, 0});
+        int[] arrayInvertidoMurilo = array.inverterMurilo(arr);
         array.print(arrayInvertidoMurilo);
     }
 }
